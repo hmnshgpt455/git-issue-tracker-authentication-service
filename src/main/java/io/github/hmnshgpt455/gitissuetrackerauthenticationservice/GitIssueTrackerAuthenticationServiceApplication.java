29d5519a50a5a0b4d7d9925456a0400f3819ae7c
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-05-17 12:14:27 +0530

    Adding initial project and circle ci config. Closes #3.

---
+++ src/main/java/io/github/hmnshgpt455/gitissuetrackerauthenticationservice/GitIssueTrackerAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.hmnshgpt455.gitissuetrackerauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIssueTrackerAuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitIssueTrackerAuthenticationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/hmnshgpt455/gitissuetrackerauthenticationservice/GitIssueTrackerAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hmnshgpt455.gitissuetrackerauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIssueTrackerAuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
