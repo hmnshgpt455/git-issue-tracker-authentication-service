28b45c62dc625fef6b12a1bfb95a1af02b20ffa1
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-05-18 13:04:13 +0530

    Removing unique password constraint. Closes #11.

--- src/main/java/io/github/hmnshgpt455/gitissuetrackerauthenticationservice/domain/UserAuthentication.java
+++ src/main/java/io/github/hmnshgpt455/gitissuetrackerauthenticationservice/domain/UserAuthentication.java
@@ -35,7 +35,7 @@ public class UserAuthentication {
     @UpdateTimestamp
     private Timestamp lastModifiedDate;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private String password;
 
     @Column(unique = true, nullable = false)
